7120baffe42b5953e24c321f7ab642ae4afca016
Leonard Moses <qalexeon@Xavier.lan>
2021-10-05 15:00:10 -0400

    remove button working. Add button added and working.

--- controllers/controller1.js
+++ controllers/controller1.js
@@ -49,14 +49,21 @@ controller1.delete('/show_personal/:id', (req, res) => {
 
 
 // UPDATE (put)
-controller1.put('/show_personal/:id' , (req ,res) => {
+controller1.put('/show_personal/add/:id' , (req ,res) => {
     
-	UserRoutine.findOneAndUpdate(req.params.id, {$inc:{sets:-1}}, {new: true}, (error, updatedWorkout) => {
-        console.log(updatedWorkout)
+	UserRoutine.findByIdAndUpdate(req.params.id, {$inc:{sets:+1}}, {new: true}, (error, updatedWorkout) => {
+        console.log(req.params.id)
 		res.redirect('/index/show_personal');
 	});
 });
 
+controller1.put('/show_personal/remove/:id' , (req ,res) => {
+    
+	UserRoutine.findByIdAndUpdate(req.params.id, {$inc:{sets:-1}}, {new: true}, (error, updatedWorkout) => {
+        console.log(req.params.id)
+		res.redirect('/index/show_personal');
+	});
+});
 
 
 // CREATE (post)
