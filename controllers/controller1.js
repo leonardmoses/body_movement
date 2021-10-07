966cbb3ff0b2072a4217fc81c44a5fff1c0f0858
Leonard Moses <qalexeon@Xavier.lan>
2021-10-07 15:42:57 -0400

    changed all files to eliminate index

--- controllers/controller1.js
+++ controllers/controller1.js
@@ -35,26 +35,26 @@ controller1.get('/' , (req,res) => { /* Test route to index */
 })
 
 // NEW (get)
-controller1.get('/index/new' , (req,res) => { 
+controller1.get('/new' , (req,res) => { 
     res.render('new.ejs' );
 })
 
 // DESTROY (delete)
-controller1.delete('/index/show_personal/:id', (req, res) => {
+controller1.delete('/show_personal/:id', (req, res) => {
     UserRoutine.findByIdAndRemove(req.params.id, (err, data) => {
     // res.send('deleting...'); /* testing delete route */
-    res.redirect('/index/show_personal');
+    res.redirect('/show_personal');
     });
 });
 
 
 // UPDATE (put)
     //Manual Editing
-controller1.put('/index/show_personal/edit/:id', (req, res) => {  
+controller1.put('/show_personal/edit/:id', (req, res) => {  
 	UserRoutine.findByIdAndUpdate(req.params.id, req.body , {new: true}, 
         (error, updatedWorkout) => {
         console.log(updatedWorkout)
-        res.redirect(`/index/show_personal`);
+        res.redirect(`/show_personal`);
     });
     // UserRoutine.findByIdAndUpdate(req.params.id, req.body)
     // .then((updatedWorkout) => {
@@ -64,44 +64,44 @@ controller1.put('/index/show_personal/edit/:id', (req, res) => {
 });
 
     //Adding a Set
-controller1.put('/index/show_personal/add/:id' , (req ,res) => {
+controller1.put('/show_personal/add/:id' , (req ,res) => {
     
 	UserRoutine.findByIdAndUpdate(req.params.id, {$inc:{sets:+1}}, {new: true}, (error, updatedWorkout) => {
         console.log(req.params.id)
-		res.redirect('/index/show_personal');
+		res.redirect('/show_personal');
 	});
 });
     //Removing a Set
-controller1.put('/index/show_personal/remove/:id' , (req ,res) => {
+controller1.put('/show_personal/remove/:id' , (req ,res) => {
     
 	UserRoutine.findByIdAndUpdate(req.params.id, {$inc:{sets:-1}}, {new: true}, (error, updatedWorkout) => {
         console.log(req.params.id)
-		res.redirect('/index/show_personal');
+		res.redirect('/show_personal');
 	});
 });
 
 
 // CREATE (post)
-controller1.post("/index/", (req, res) => {
+controller1.post("/", (req, res) => {
     UserRoutine.create(req.body, (error, createdWorkout) => {
-        res.redirect("/index/show_personal");
+        res.redirect("/show_personal");
     });
 });
 
 // EDIT (get) (put)
-controller1.get('/index/edit_personal' , (req,res) => { /* Test route to edit */
+controller1.get('/edit_personal' , (req,res) => { /* Test route to edit */
     UserRoutine.find({}, (error, allworkouts) => {
     res.render('edit_personal.ejs' , {workouts: allworkouts});
     });
 });
 
 // SHOW (get)
-controller1.get('/index/show_preset' , (req,res) => { 
+controller1.get('/show_preset' , (req,res) => { 
     res.render('show_preset.ejs' , {workouts: preset_routine});
 })
 
 
-controller1.get('/index/show_personal' , (req,res) => {
+controller1.get('/show_personal' , (req,res) => {
     UserRoutine.find({}, (error, allworkouts) => {
         res.render('show_personal.ejs', {workouts: allworkouts});
     });
